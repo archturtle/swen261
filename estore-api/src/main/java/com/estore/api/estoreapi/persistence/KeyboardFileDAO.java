08f503b21fd0038a6802fc165bdbb2c15e4df3b1
Siddhartha Juluru <ssj4651@rit.edu>
2023-02-19 21:02:04 -0500

    Created the KeyboardDAO and added stubbed out methods to KeyboardFileDAO.

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/KeyboardFileDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/KeyboardFileDAO.java
@@ -1,6 +1,10 @@
 package com.estore.api.estoreapi.persistence;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Component;
+
+import com.estore.api.estoreapi.model.Keyboard;
 
 /**
  * Implements the functionality for JSON file-based peristance for Keyboard
@@ -12,5 +16,40 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class KeyboardFileDAO implements KeyboardDAO {
+  @Override
+  public Keyboard[] getAllKeyboards() throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Keyboard[] findKeyboardsByName(String containsText) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Keyboard getKeyboardById(int id) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Keyboard createKeyboard(Keyboard keyboard) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Keyboard updateKeyboard(Keyboard keyboard) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean deleteKeyboard(int id) throws IOException {
+    // TODO Auto-generated method stub
+    return false;
+  }
    
 }
