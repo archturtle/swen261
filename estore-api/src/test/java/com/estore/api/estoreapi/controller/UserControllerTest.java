7eb46c6b1eb8684065af3d0a283714e6f281a812
Issac Kim <idk4565@rit.edu>
2023-03-08 21:56:30 -0500

    Added more tests to UserControllerTest.java

--- estore-api/src/test/java/com/estore/api/estoreapi/controller/UserControllerTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/UserControllerTest.java
@@ -9,29 +9,31 @@ import java.io.IOException;
 import java.security.Key;
 
 import com.estore.api.estoreapi.persistence.GenericDAO;
+import com.estore.api.estoreapi.persistence.UserFileDAO;
 import com.estore.api.estoreapi.model.User;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Io;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @Tag("Controller-Tier")
 public class UserControllerTest {
     private UserController userController;
-    private GenericDAO mockGenericDao;
+    private UserFileDAO mockUserFileDao;
 
     @BeforeEach
     public void setupUserController(){
-        mockGenericDao = mock(GenericDAO.class);
-        userController = new UserController(mockGenericDao);
+        mockUserFileDao = mock(UserFileDAO.class);
+        userController = new UserController(mockUserFileDao);
     }
 
     @Test
     public void testGetUser() throws IOException{
         User testUser = new User(0, "Issac", 0);
-        when(mockGenericDao.findByID(testUser.getId())).thenReturn(testUser);
+        when(mockUserFileDao.findByID(testUser.getId())).thenReturn(testUser);
 
         ResponseEntity<User> response = userController.getUser(testUser.getId());
 
@@ -43,7 +45,7 @@ public class UserControllerTest {
     public void testGetUserNotFound() throws IOException{
         int userID = 1;
 
-        when(mockGenericDao.findByID(userID)).thenReturn(null);
+        when(mockUserFileDao.findByID(userID)).thenReturn(null);
 
         ResponseEntity<User> response = userController.getUser(userID);
 
@@ -54,7 +56,7 @@ public class UserControllerTest {
     public void testGetUserHandleExceptions() throws Exception{
         int userID = 1;
 
-        doThrow(new IOException()).when(mockGenericDao).findByID(userID);
+        doThrow(new IOException()).when(mockUserFileDao).findByID(userID);
 
         ResponseEntity<User> response = userController.getUser(userID);
 
@@ -65,7 +67,7 @@ public class UserControllerTest {
     public void testCreateUser() throws IOException{
         User testUser = new User(0, "Issac", 0);
 
-        when(mockGenericDao.create(testUser)).thenReturn(testUser);
+        when(mockUserFileDao.create(testUser)).thenReturn(testUser);
 
         ResponseEntity<User> response = userController.createUser(testUser);
 
@@ -77,7 +79,7 @@ public class UserControllerTest {
     public void testCreateUserFailed() throws IOException{
         User testUser = new User(0, "Issac", 0);
 
-        when(mockGenericDao.create(testUser)).thenReturn(null);
+        when(mockUserFileDao.create(testUser)).thenReturn(null);
 
         ResponseEntity<User> response = userController.createUser(testUser);
 
@@ -88,7 +90,7 @@ public class UserControllerTest {
     public void testCreateUserHandleException() throws IOException{
         User testUser = new User(0, "Issac", 0);
 
-        doThrow(new IOException()).when(mockGenericDao).create(testUser);
+        doThrow(new IOException()).when(mockUserFileDao).create(testUser);
 
         ResponseEntity<User> response = userController.createUser(testUser);
 
@@ -99,7 +101,7 @@ public class UserControllerTest {
     public void testUpdateUser() throws IOException{
         User testUser = new User(0, "Issac", 0);
         
-        when(mockGenericDao.update(testUser)).thenReturn(testUser);
+        when(mockUserFileDao.update(testUser)).thenReturn(testUser);
         ResponseEntity<User> response = userController.updateUser(testUser);
         testUser.setName("Chan");
 
@@ -107,5 +109,41 @@ public class UserControllerTest {
         
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(testUser, response.getBody());
+    }
+
+    @Test
+    public void testUpdateUserFail() throws IOException{
+        User testUser = new User(0, "Issac", 0);
+
+        when(mockUserFileDao.update(testUser)).thenReturn(null);
+
+        ResponseEntity<User> response = userController.updateUser(testUser);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void testUpdateUserHandleException() throws IOException{
+        User testUser = new User(0, "Issac", 0);
+
+        doThrow(new IOException()).when(mockUserFileDao).update(testUser);
+
+        ResponseEntity<User> response = userController.updateUser(testUser);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+    @Test
+    public void testGetUsers() throws IOException{
+        User[] testUsers = new User[2];
+        testUsers[0] = new User(0, "Issac", 0);
+        testUsers[1] = new User(1, "Poopyface", 1);
+
+        when(mockUserFileDao.getAll()).thenReturn(testUsers);
+
+        ResponseEntity<User[]> response = userController.getUsers();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(testUsers, response.getBody());
     }
 }
