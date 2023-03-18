ed2e40338cf74218bb7086fa391e2c7a4009f156
Siddhartha Juluru <ssj4651@rit.edu>
2023-03-18 16:56:11 -0400

    Created the product service

--- estore-ui/src/app/services/products.service.ts
+++ estore-ui/src/app/services/products.service.ts
@@ -1,10 +1,37 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, map, Observable, tap } from 'rxjs';
+import { Keyboard } from '../interfaces/keyboard';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  private _products: BehaviorSubject<Keyboard[]> = new BehaviorSubject<Keyboard[]>([]);
+  public readonly products$: Observable<Keyboard[]> = this._products.asObservable();
 
-  constructor() { }
+  constructor(private httpService: HttpClient) { }
+
+  getProducts$(name: string): Observable<Keyboard[]> {
+    const url = (name != null) ? "http://localhost:8080/keyboards" : `http://localhost:8080/keyboards/?name=${name}`;
+
+    return this.httpService.get(url)
+      .pipe(
+        map((result: any) => {
+          return result.map((r: any) => {
+            return {
+              id: r["id"],
+              name: r["name"],
+              price: r["price"],
+              quantity: r["quantity"]
+            }
+          });
+        }),
+        tap({
+          next: (value: Keyboard[]) => {
+            this._products.next(value);
+          }
+        })
+      )
+  }
 }
