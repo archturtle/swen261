34db4566c7ef8e17f7eb8d6fb4f9d4b08151986a
Siddhartha Juluru <ssj4651@rit.edu>
2023-03-20 15:18:17 -0400

    Created the login component ui

--- estore-ui/src/app/components/login/login.component.ts
+++ estore-ui/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup = this.formBuilder.group({
+    username: ['']
+  });
 
+  constructor(private formBuilder: FormBuilder) {  }
 }
