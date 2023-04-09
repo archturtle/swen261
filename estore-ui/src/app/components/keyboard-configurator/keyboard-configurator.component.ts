816aaf1acd3469fce87b4643bd57ee78b7caf69d
Siddhartha Juluru <ssj4651@rit.edu>
2023-04-08 22:02:49 -0400

    fix margin collapse and some starter css

--- estore-ui/src/app/components/keyboard-configurator/keyboard-configurator.component.ts
+++ estore-ui/src/app/components/keyboard-configurator/keyboard-configurator.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, HostListener, OnDestroy, ViewChild } from '@angular/core';
+import { Subscription, debounceTime, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-keyboard-configurator',
   templateUrl: './keyboard-configurator.component.html',
   styleUrls: ['./keyboard-configurator.component.css']
 })
-export class KeyboardConfiguratorComponent {
+export class KeyboardConfiguratorComponent implements AfterViewInit, OnDestroy {
+  // Slider components
+  @ViewChild("sliderThumb") sliderThumb!: ElementRef<HTMLSpanElement>;
+  @ViewChild("sliderLayer") sliderLayer!: ElementRef<HTMLSpanElement>;
 
+  // False means case, True means Keycaps
+  thumbPosition: boolean = true;
+
+  // Privates
+  private resizeSubscription: Subscription;  
+
+  constructor() {
+    this.resizeSubscription = fromEvent(window, 'resize')
+      .pipe(debounceTime(100))
+      .subscribe(() => this.click());
+  }
+
+  ngAfterViewInit(): void { this.click(); }
+  ngOnDestroy(): void { this.resizeSubscription.unsubscribe(); }
+
+  click(): void {
+    // let sliderThumbRect: DOMRect = this.sliderThumb.nativeElement.getBoundingClientRect()
+    // let sliderLayerRect: DOMRect = this.sliderLayer.nativeElement.getBoundingClientRect();
+    
+    // if (this.thumbPosition) {
+    //   this.sliderThumb.nativeElement.style.top = (sliderLayerRect.top).toString() + "px";
+    // } else {
+    //   this.sliderThumb.nativeElement.style.top = (sliderLayerRect.bottom - (sliderThumbRect.height)).toString() + "px";
+    // }
+  }
 }
